e293d5556a9186c975dead8b82df3423b1c4b3c5
Chinelo <okpalachinelo99@gmail.com>
2023-04-22 12:16:33 +0100

    first commit

---
+++ src/dtos/request/FloInformation.java
@@ -0,0 +1,31 @@
+package dtos;
+
+public class FloInformation {
+    private int numberOfDays;
+    private int monthlyRange;
+    private int previousFirstDay;
+
+    public int getNumberOfDays() {
+        return numberOfDays;
+    }
+
+    public void setNumberOfDays(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+
+    public int getMonthlyRange() {
+        return monthlyRange;
+    }
+
+    public void setMonthlyRange(int monthlyRange) {
+        this.monthlyRange = monthlyRange;
+    }
+
+    public int getPreviousFirstDay() {
+        return previousFirstDay;
+    }
+
+    public void setPreviousFirstDay(int previousFirstDay) {
+        this.previousFirstDay = previousFirstDay;
+    }
+}
---
+++ src/dtos/request/UserRegistration.java
@@ -0,0 +1,52 @@
+package dtos;
+
+import java.time.LocalDate;
+
+public class UserRegistration {
+    private int userId;
+ private String firstName;
+  private String lastName;
+    private String passWord;
+    private LocalDate timeCreated = LocalDate.now();
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public LocalDate getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDate timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+}
